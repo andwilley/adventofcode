58e1457f0bade1100ce8411288f5d40c0b47fc4e
Drew Willey <andwilley@gmail.com>
2025-12-19 01:22:14 +0000

    day6 part1

--- 2025/day6/src/main.rs
+++ 2025/day6/src/main.rs
@@ -2,29 +2,46 @@ use std::io::{self, BufRead, Cursor};
 
 pub(crate) fn math<F>(reader: io::BufReader<Cursor<&str>>, part: F) -> io::Result<u64>
 where
-    F: Fn() -> u64,
+    F: Fn(&Vec<Vec<u64>>, &Vec<String>) -> u64,
 {
-    for line_result in reader.lines() {
+    let mut operands: Vec<Vec<u64>> = Vec::new();
+    let mut operators: Vec<String> = Vec::new();
+    let mut lines = reader.lines().peekable();
+    while let Some(line_result) = lines.next() {
         let line_untrimmed = line_result?;
         let line = line_untrimmed.trim();
+        let vals = line.split_whitespace().map(|n| n.trim());
+        if lines.peek().is_some() {
+            operands.push(vals.map(|n| n.parse().unwrap()).collect());
+        } else {
+            operators = vals.map(|s| s.to_string()).collect();
+        }
     }
-    Ok(part())
+    Ok(part(&operands, &operators))
 }
 
 #[allow(dead_code)]
-pub(crate) fn part_1() -> u64 {
-    10
+pub(crate) fn part_1(operands: &Vec<Vec<u64>>, operators: &Vec<String>) -> u64 {
+    let mut ans = 0;
+    for i in 0..operators.len() {
+        match operators[i].as_str() {
+            "*" => ans += operands.iter().fold(1, |acc, x| acc * x[i]),
+            "+" => ans += operands.iter().fold(0, |acc, x| acc + x[i]),
+            unknown_op => panic!("unsupported operator {unknown_op}"),
+        }
+    }
+    ans
 }
 
 #[allow(dead_code)]
-pub(crate) fn part_2() -> u64 {
+pub(crate) fn part_2(_: &Vec<Vec<u64>>, _: &Vec<String>) -> u64 {
     10
 }
 
 fn main() -> io::Result<()> {
     let input = include_str!("../data/input.txt");
     let reader = io::BufReader::new(Cursor::new(input));
-    let ans = math(reader, part_2)?;
+    let ans = math(reader, part_1)?;
     println!("{ans}");
     Ok(())
 }
